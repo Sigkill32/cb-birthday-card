3d2f925a292c70b9c86e1599c00fcb7ce6d970c4
Manoj Bhat <bhatanm@gmail.com>
2019-12-05 12:22:07 +0530

    print friendliness added

--- src/components/birthdayCard.jsx
+++ src/components/birthdayCard.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Person from "./person";
+import card from "../images/card.jpg";
 
 const BirthdayCard = ({ reason, onHandleClick }) => {
   return (
     <div className="birthday-card" onClick={onHandleClick}>
+      <img src={card} alt="" />
       <Person />
       <h2>{reason}</h2>
     </div>
