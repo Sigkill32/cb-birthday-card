4b8eb7f58b4e474e1ec086556ee9b59610f0a983
Manoj Bhat <bhatanm@gmail.com>
2020-01-23 15:08:46 +0530

    Added Styles

--- src/components/birthdayCard.jsx
+++ src/components/birthdayCard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Person from "./person";
-// import card from "../images/card.jpg";
 import left from "../images/left.svg";
 import right from "../images/right.svg";
 import Controls from "./controls";
@@ -25,18 +24,25 @@ const BirthdayCard = ({
   cardValue
 }) => {
   return (
-    <div className="birthday-card">
-      <img src={require(`../images/${cardValue}.jpg`)} alt="" />
-      <Person imgUrl={imgUrl} angle={angle} top={top} leftPos={leftPos} rangeValue={rangeValue} imgWidth/>
+    <div className='birthday-card'>
+      <img src={require(`../images/${cardValue}.jpg`)} alt='' />
+      <Person
+        imgUrl={imgUrl}
+        angle={angle}
+        top={top}
+        leftPos={leftPos}
+        rangeValue={rangeValue}
+        imgWidth
+      />
       <h2>{reason}</h2>
-      <div className="rotate-buttons">
+      <div className='rotate-buttons'>
         <button onClick={onHandleRotateLeft}>
-          <img src={left} alt="rotate left" />
+          <img src={left} alt='rotate left' />
         </button>
         <button onClick={onHandleRotateRight}>
-          <img src={right} alt="rotate right" />
+          <img src={right} alt='rotate right' />
         </button>
-        <button onClick={onHandleClick} className="back-button">
+        <button onClick={onHandleClick} className='back-button'>
           Back
         </button>
       </div>
